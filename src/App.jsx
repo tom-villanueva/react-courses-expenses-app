bc78edc2ea93fdd52c633080ee2200543bd54748
Tomás Villanueva Jousset <tvillanueva@justierradelfuego.gov.ar>
2023-06-15 10:08:26 -0300

    Cambios clase 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,50 @@ import "./App.css";
 import BudgetExpenseForm from "./components/BudgetExpenseForm";
 import BudgetList from "./components/BudgetList";
 import BudgetSummary from "./components/BudgetSummary";
+import { useState } from "react";
+
+const generateUniqueID = (array) => {
+  let uniqueID;
+  const usedIDs = new Set();
+
+  do {
+    uniqueID = Math.floor(Math.random() * 1000);
+  } while (usedIDs.has(uniqueID) || uniqueID.toString().length > 3);
+
+  usedIDs.add(uniqueID);
+
+  return uniqueID;
+};
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, description: "Gasto 1", amount: 1000, isImportant: false },
     { id: 2, description: "Gasto 2", amount: 500, isImportant: true },
-  ];
+  ]);
+
+  const onAddExpense = (expense) => {
+    const newExpense = {
+      id: generateUniqueID(expenses),
+      ...expense,
+    };
+
+    const newExpenses = [...expenses, newExpense];
+
+    setExpenses(newExpenses);
+  };
+
+  const totalSpent = () => {
+    return expenses.reduce((acc, expense) => acc + expense.amount, 0);
+  };
 
   return (
     <div className="budget-manager">
       <h1>Budget Manager</h1>
-      <BudgetSummary total={8000} spent={1500} />
+      <BudgetSummary total={8000} spent={totalSpent()} />
       <h3>Expenses</h3>
       <BudgetList expenses={expenses} />
       <h3>Add new Expense</h3>
-      <BudgetExpenseForm />
+      <BudgetExpenseForm onAddExpense={onAddExpense} />
     </div>
   );
 }
--- src/components/BudgetExpenseForm.jsx
+++ src/components/BudgetExpenseForm.jsx
@@ -1,10 +1,46 @@
-const BudgetExpenseForm = () => {
+import { useState } from "react";
+
+const BudgetExpenseForm = ({ onAddExpense }) => {
+  const [expense, setExpense] = useState({
+    description: "",
+    amount: "",
+  });
+
+  const handleChange = (e) => {
+    setExpense({
+      ...expense,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newExpense = {
+      ...expense,
+      amount: Number(expense.amount),
+    };
+
+    onAddExpense(newExpense);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <label htmlFor="description">Description</label>
-      <input id="description" name="description" type="text" />
+      <input
+        id="description"
+        name="description"
+        type="text"
+        value={expense.description}
+        onChange={handleChange}
+      />
       <label htmlFor="amount">Amount Spent</label>
-      <input id="amount" name="amount" type="number" />
+      <input
+        id="amount"
+        name="amount"
+        type="number"
+        value={expense.amount}
+        onChange={handleChange}
+      />
       <button type="submit">Save</button>
     </form>
   );
